5fb3f639614c86b0ab8c44a89b069ae2c7361e5b
coma8765 <coma8765my@gmail.com>
2022-06-07 01:00:22 +0500

    Remove print storage path

--- app/settings.py
+++ app/settings.py
@@ -5,6 +5,5 @@ JWT_SECRET = os.getenv("JWT_SECRET", "")
 JWT_ALG = "HS256"
 
 STORAGE = pathlib.Path(os.getenv("STORAGE", "/tmp"))
-print(STORAGE)
 STORAGE_PRESENTATION_IMAGES = \
     STORAGE / os.getenv("STORAGE_PRESENTATION_IMAGES", "slides")
